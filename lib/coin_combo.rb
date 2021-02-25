cdd11f06da7353ab4b4d91fbe131efec62d3af0b
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-25 13:34:14 -0800

    Alter fourth test to return all types of change. Pass.
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -4,9 +4,9 @@ class Coins
     75
     quarters = (cents / 25).floor
     dimes = ((cents - (quarters*25)) / 10).floor
-    nickles = ((cents - ((quarters*25)+(dimes*10))) / 5).floor
-    # pennies = cents - (quarters*25) - (dimes*10) - (nickels*5)
-  coins_array = [quarters, dimes, nickles]
+    nickels = ((cents - ((quarters*25)+(dimes*10))) / 5).floor
+    pennies = cents - ((quarters*25) + (dimes*10) + (nickels*5))
+  coins_array = [quarters, dimes, nickels, pennies]
   # until index > 0 
   #   "#{index}"
   end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -5,18 +5,18 @@ require('pry')
 describe('Coins#combo') do
   it("It will return the number of quarters needed for change") do
     cents = Coins.new()
-    expect(cents.combo(0.75)).to(eq([3,0,0]))
+    expect(cents.combo(0.75)).to(eq([3,0,0,0]))
   end
   it("It will return the number of quarters and dimes needed for change") do
     cents = Coins.new()
-    expect(cents.combo(0.85)).to(eq([3,1,0]))
+    expect(cents.combo(0.85)).to(eq([3,1,0,0]))
   end
   it("It will return the number of quarters, dimes and nickles needed to make that change") do
     cents = Coins.new()
-    expect(cents.combo(0.90)).to(eq([3,1,1]))
+    expect(cents.combo(0.90)).to(eq([3,1,1,0]))
   end
-  # it("It will return the number of dimes and nickles and pennies needed to make that change") do
-  #   cents = Coins.new()
-  #   expect(cents.combo(0.16)).to(eq("You need 1 dimes 1 nickels and 1 pennies."))
-  # end
+  it("It will return the number of quarters, dimes, nickels and pennies needed to make that change") do
+    cents = Coins.new()
+    expect(cents.combo(0.91)).to(eq([3,1,1,1]))
+  end
 end
