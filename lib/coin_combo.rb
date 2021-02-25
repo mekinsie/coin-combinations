b236a4c848c0bdda1231faf212c45e75bfb2f06b
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-25 14:06:38 -0800

    Add test to return all coins in a string. Pass.
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,13 +1,30 @@
 class Coins
   def combo(cents)
-    cents *= 100
-    75
-    quarters = (cents / 25).floor
-    dimes = ((cents - (quarters*25)) / 10).floor
-    nickels = ((cents - ((quarters*25)+(dimes*10))) / 5).floor
-    pennies = cents - ((quarters*25) + (dimes*10) + (nickels*5))
-  coins_array = [quarters, dimes, nickels, pennies]
-  # until index > 0 
-  #   "#{index}"
+    change_calc(cents)
+    counter = 0
+    total_change_array = []
+    @coins_array.each do |coin|
+      if coin > 0 && counter == 0
+        total_change_array.push("#{coin} quarters")
+      elsif coin > 0 && counter == 1
+        total_change_array.push("#{coin} dimes")
+      elsif coin > 0 && counter == 2
+        total_change_array.push("#{coin} nickels")
+      elsif coin > 0 && counter == 3
+        total_change_array.push("#{coin} pennies")
+      end
+      counter += 1
+    end
+    total_change_array.join(", ")
   end
+
+  # private
+    def change_calc(cents)
+      cents *= 100
+      quarters = (cents / 25).floor
+      dimes = ((cents - (quarters*25)) / 10).floor
+      nickels = ((cents - ((quarters*25)+(dimes*10))) / 5).floor
+      pennies = (cents - ((quarters*25) + (dimes*10) + (nickels*5))).floor
+      @coins_array = [quarters, dimes, nickels, pennies]
+    end
 end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -5,18 +5,26 @@ require('pry')
 describe('Coins#combo') do
   it("It will return the number of quarters needed for change") do
     cents = Coins.new()
-    expect(cents.combo(0.75)).to(eq([3,0,0,0]))
+    expect(cents.change_calc(0.75)).to(eq([3,0,0,0]))
   end
   it("It will return the number of quarters and dimes needed for change") do
     cents = Coins.new()
-    expect(cents.combo(0.85)).to(eq([3,1,0,0]))
+    expect(cents.change_calc(0.85)).to(eq([3,1,0,0]))
   end
   it("It will return the number of quarters, dimes and nickles needed to make that change") do
     cents = Coins.new()
-    expect(cents.combo(0.90)).to(eq([3,1,1,0]))
+    expect(cents.change_calc(0.90)).to(eq([3,1,1,0]))
   end
   it("It will return the number of quarters, dimes, nickels and pennies needed to make that change") do
     cents = Coins.new()
-    expect(cents.combo(0.91)).to(eq([3,1,1,1]))
+    expect(cents.change_calc(0.91)).to(eq([3,1,1,1]))
+  end
+  it("It will return the number of quarters, dimes, nickels and pennies needed to make that change in a string") do
+    cents = Coins.new()
+    expect(cents.combo(0.91)).to(eq("3 quarters, 1 dimes, 1 nickels, 1 pennies"))
+  end
+  it("It will return the number of quarters, dimes, nickels and pennies needed to make that change in a string") do
+    cents = Coins.new()
+    expect(cents.combo(0.75)).to(eq("3 quarters"))
   end
 end
