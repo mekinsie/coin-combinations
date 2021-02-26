d989a096fbf18c3b8c5cfd96ed51afe709a6543c
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-25 16:18:50 -0800

    Write script file for logic.
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,17 +1,18 @@
 class Coins
+
   def combo(cents)
     change_calc(cents)
     counter = 0
     total_change_array = []
     @coins_array.each do |coin|
       if coin > 0 && counter == 0
-        total_change_array.push("#{coin} quarters")
+        total_change_array.push("#{coin} quarter(s)")
       elsif coin > 0 && counter == 1
-        total_change_array.push("#{coin} dimes")
+        total_change_array.push("#{coin} dime(s)")
       elsif coin > 0 && counter == 2
-        total_change_array.push("#{coin} nickels")
+        total_change_array.push("#{coin} nickel(s)")
       elsif coin > 0 && counter == 3
-        total_change_array.push("#{coin} pennies")
+        total_change_array.push("#{coin} penny(s)")
       end
       counter += 1
     end
--- lib/script.rb
+++ lib/script.rb
@@ -1,3 +1,8 @@
 #! usr/bin/env ruby
+require_relative("coin_combo.rb")
 
-puts "Hello World!"
+puts "Welcome to the coin combination app!"
+puts "Please type a cent amount in decimals"
+input = gets.chomp
+change = Coins.new()
+puts change.combo(input.to_f)
